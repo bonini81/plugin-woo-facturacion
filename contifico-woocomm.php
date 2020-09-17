17cafa68d18a2ebe0b5091a333cd4c716a5a2266
bonini81 <andresdominguez81@gmail.com>
2020-09-17 16:50:19 -0500

    Agregue el plugin header

---
+++ contifico-woocomm.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Integración Contifico y Woocommerce 
+Description: Integrar Woocommerce con Contifico
+Plugin URI:    Soon...
+Author:      Msc. Andrés Domínguez Bonini 
+Version:     1.0
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
